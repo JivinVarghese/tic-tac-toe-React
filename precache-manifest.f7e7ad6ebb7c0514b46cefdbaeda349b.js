b9a685292dce3d85acf7a5c01c4177b3de96e0c1
Jivin Varghese <jivinvarghese007@gmail.com>
2020-10-23 20:15:26 +0530

    Updates

---
+++ precache-manifest.f7e7ad6ebb7c0514b46cefdbaeda349b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e265e7803d3d7c7fe8e0099c2d55daf3",
+    "url": "/tic-tac-toe-React/index.html"
+  },
+  {
+    "revision": "73bb58864a9d621af80e",
+    "url": "/tic-tac-toe-React/static/css/main.55ca0676.chunk.css"
+  },
+  {
+    "revision": "815361ded309b2ad7c6e",
+    "url": "/tic-tac-toe-React/static/js/2.39d1e469.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tic-tac-toe-React/static/js/2.39d1e469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bb58864a9d621af80e",
+    "url": "/tic-tac-toe-React/static/js/main.f6309926.chunk.js"
+  },
+  {
+    "revision": "2b805a53af8a3ef95974",
+    "url": "/tic-tac-toe-React/static/js/runtime-main.3b1e3711.js"
+  }
+]);
